1ed8cecff579458d2361afd20a9c51e7e200a513
MaverickMaddy11 <chopramadhav2004@gmail.com>
2025-04-29 20:05:07 +0530

    secondcommit

---
+++ packages/common/src/index.ts
@@ -0,0 +1,18 @@
+import z from "zod";
+
+export const CreateUserSchema = z.object({
+  username: z.string(),
+  password: z.string(),
+  name: z.string(),
+});
+
+export const SigninSchema = z.object({
+  username: z.string(),
+  password: z.string(),
+  name: z.string(),
+});
+
+export const CreateRoom = z.object({
+  name: z.string(),
+  password: z.string(),
+});
